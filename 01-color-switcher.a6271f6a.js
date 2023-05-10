9e51ed7fce326844eb77b35a5dd9656114b82357
BadenIra2023 <123290328+BadenIra2023@users.noreply.github.com>
2023-05-10 06:49:40 +0000

    Deploying to gh-pages from @ BadenIra2023/goit-js-hw--09@c3fbe090aeebc4a6025b092b4acab526b34dc0a6 🚀

---
+++ 01-color-switcher.a6271f6a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(l){a=setInterval((()=>{e.disabled=!0,t.disabled=!1,d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(function(d){clearInterval(a),e.disabled=!1,t.disabled=!0}));let a=null;t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.a6271f6a.js.map
